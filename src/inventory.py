683a80f68f388e7adbd6b19f79a4476eb5131051
Blair Fernandes <31095601+blair49@users.noreply.github.com>
2024-08-24 16:50:11 +0530

    Add functions to update, remove items and view total value of inventory

--- src/inventory.py
+++ src/inventory.py
@@ -1,11 +1,43 @@
 from item import Item
 class Inventory:
     def __init__(self) -> None:
-        self.items = []
+        self.items = {}
     
     def add_item(self, newItem:Item):
         if newItem:
-            self.items.append(newItem)
+            self.items[newItem.name] = newItem
     
+    def update_item(self, name:str, quantity:int=None, price:float=None):
+        if self.items[name]:
+            if quantity is not None:
+                self.items[name].quantity=quantity
+            if price is not None:
+                self.items[name].price=price
+
+    def remove_item(self, name):
+        if name in self.items:
+            del self.items[name]
+
+    def low_stock(self, threshold:int):
+        print("---------")
+        print("Low stock items")
+        print("---------")
+        for item in self.items:
+            if self.items[item].quantity < threshold:
+                print(self.items[item])
+        print("---------")
+        
+    def total_value(self)->float:
+        total = 0
+        for item in self.items.values():
+            total += item.price * item.quantity
+        return total
+
     def view_inventory(self):
-        print(f"Items {self.items}")
+        print("---------")
+        print("Inventory")
+        print("---------")
+        for item in self.items:
+            print(self.items[item])
+        print("---------")
+        
